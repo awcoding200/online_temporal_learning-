e3804c204293810915dbb936244ed8d7d9518792
aw <aw@example.com>
2025-03-31 00:58:06 +0200

    add equation 21

--- src/libOTL/otl_sogp.cpp
+++ src/libOTL/otl_sogp.cpp
@@ -97,7 +97,7 @@ namespace OTL
         }
         VectorXd ehat = this->Q.block(0, 0, this->current_size, this->current_size) * k;
 
-        double gamma = kstar - k.dot(ehat);
+        double gamma = kstar - k.dot(ehat);  // equation 21
         double eta = 1.0 / (1.0 + gamma * r);
 
         if (gamma < 1e-12)
